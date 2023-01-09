1e62b22a05a1f1ce00cd36a0017877dd8f37b788
vanman9599 <vanjordan46@hotmail.com>
2023-01-09 05:56:28 -0800

    reszied divs, images

--- components/Article.js
+++ components/Article.js
@@ -4,13 +4,13 @@ const Article = (props) =>{
 
     return(
         <div className="container-fluid article">
-          <div className="row justify-content-evenly m-auto">
+          <div className="row m-auto">
             <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12 p-5">
             
                 <h1>{props.title}</h1>
                 <img className="image1" src={props.pic} />
                 <img className="image2" src={props.pic_mobile} />
-                <p>{props.article}</p>
+                {/* <p className="article-text">{props.article}</p> */}
             </div>
         </div>
     </div>
