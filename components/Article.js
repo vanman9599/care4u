58fea3ee46edce7be23f2f992dc343abba0a5bef
vanman9599 <vanjordan46@hotmail.com>
2023-01-08 21:01:50 -0800

    article styling

--- components/Article.js
+++ components/Article.js
@@ -5,11 +5,11 @@ const Article = (props) =>{
     return(
         <div className="container-fluid article">
           <div className="row justify-content-evenly m-auto">
-            <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+            <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12 p-5">
             
                 <h1>{props.title}</h1>
                 <img src={props.pic} />
-                <p>{props.article}</p>
+                <p className="article-text">{props.article}</p>
             </div>
         </div>
     </div>
