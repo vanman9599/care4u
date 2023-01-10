68eb7917603d2be5eae6293fa20608b999b3656e
vanman9599 <vanjordan46@hotmail.com>
2023-01-09 21:16:42 -0800

    switch logo

--- components/DogFoodListing.js
+++ components/DogFoodListing.js
@@ -1,3 +1,4 @@
+import { goodreads } from 'fontawesome';
 import react from 'react';
 import dogfoods from './dogfoods';
 
@@ -18,13 +19,17 @@ const DogFoodListing = ()=>{
             <p className="description">{food.description}</p>
           </div>
           <div className="col-lg-3 col-md-3 col-sm-3 col-xs-3 text-center">
-             <img key={food.foodname} src={food.pic} />
+            <a href={food.link} >
+             <img key={food.foodname} alt={food.foodname} src={food.pic} />
+             </a>
           </div>
           <div className="col-lg-3 col-md-3 col-sm-3 col-xs-3 promo text-center">
              <p>{food.promo}</p>
+             <a href={food.link} >
              <button type="button" className="btn goButton">
                 Get {food.foodname} now!
                 </button>
+                </a>
           </div>
         </div>
     </div>
--- components/dogfoods.js
+++ components/dogfoods.js
@@ -4,9 +4,9 @@ const dogfoods = [
         title: "Best Value!",
         ranking: 4.7,
         description: "Lorem ipsum dolor sit amet, congue vocent accommodare et pri, ex suas iisque nusquam qui. Id nam partem facilis qualisque, eam dicit veritus eu. At mea aeque delicatissimi, cu dicit democritum mei, sumo decore voluptua sed ne. Mutat sonet utinam sea in, ut vis singulis postulant liberavisse. Soleat nemore detraxit cu mel. Pro id quas fugit.",
-        pic: "nomnom.gif", 
-        promo: "Get 50% your first box",
-        link: "nomnomnow.com" 
+        pic: "https://ad.linksynergy.com/fs-bin/show?id=6ZzaL2n*TA4&bids=1208241.16&subid=0&type=4&gridnum=11", 
+        promo: "Get 50% your first order",
+        link: "https://click.linksynergy.com/fs-bin/click?id=6ZzaL2n*TA4&offerid=1208241.16&subid=0&type=4" 
 
     }, 
     {
