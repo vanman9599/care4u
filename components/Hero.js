4d0460bd4a8765e7a018d1d94a8abc24140c39a6
vanman9599 <vanjordan46@hotmail.com>
2023-01-10 19:28:10 -0800

    hero

--- components/Hero.js
+++ components/Hero.js
@@ -4,12 +4,19 @@ const Hero = () => {
     return(
       <>
       
-      
+      <div className="container-fluid headline">
+          <div className="row">
+            <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center">
+              Make the switch to FRESH dog food!
+              
+            </div>
+          </div>
+        </div>
     
         <div className="container-fluid image1">
           <div className="row">
-            <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12 ">
-              
+            <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center">
+             
               
             </div>
           </div>
