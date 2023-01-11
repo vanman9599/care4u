4ae84b93c912a2628e76dab1487ce79c3a1e2512
vanman9599 <vanjordan46@hotmail.com>
2023-01-11 00:53:17 -0800

    styling

--- components/Navbar.js
+++ components/Navbar.js
@@ -30,9 +30,7 @@ const Navbar = () => {
           onClick={() => setNavActive(!navActive)}
           className={`nav__menu-bar`}
         >
-          <div></div>
-          <div></div>
-          <div></div>
+        
         </div>
         <div className={`${navActive ? "active" : ""} nav__menu-list`}>
           {MENU_LIST.map((menu, idx) => (
